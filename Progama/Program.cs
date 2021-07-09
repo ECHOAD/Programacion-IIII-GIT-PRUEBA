cfbf424a41c4df41e90007c320768c53a98e6c90
Stuart Estevez <stuart264.se@gmail.com>
2021-07-09 16:36:24 -0400

    Cambio del proyecto exitoso!

--- Progama/Program.cs
+++ Progama/Program.cs
@@ -6,7 +6,7 @@ namespace Progama
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Rain!");
         }
     }
 }
